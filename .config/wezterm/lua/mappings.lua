63665140b186ca28c81457ca2bd625bc887d8712
Thomas Quan <dev.thomasquan@gmail.com>
2026-02-08 17:25:41 -0500

    Update wezterm to use mod of crtl + a

--- .config/wezterm/lua/mappings.lua
+++ .config/wezterm/lua/mappings.lua
@@ -15,28 +15,38 @@ end
 
 local M = {}
 
-local leader = { mods = mod.c, key = "a", timeout_miliseconds = 1000 }
+-- CTRL-A as the leader key
+local leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 
 local keys = function()
   local keys = {
-    -- CTRL-A, CTRL-A sends CTRL-A
-    keybind({ mod.l, mod.c }, "a", act.SendString "\x01"),
-
-    -- pane and tabs
-    keybind({ mod.l }, "ö", act.SplitVertical { domain = "CurrentPaneDomain" }),
-    keybind({ mod.l }, "ä", act.SplitHorizontal { domain = "CurrentPaneDomain" }),
-    keybind({ mod.l }, "z", act.TogglePaneZoomState),
-    keybind({ mod.l }, "c", act.SpawnTab "CurrentPaneDomain"),
+    -- Pane movement using CTRL-A + h/j/k/l
     keybind({ mod.l }, "h", act.ActivatePaneDirection "Left"),
     keybind({ mod.l }, "j", act.ActivatePaneDirection "Down"),
     keybind({ mod.l }, "k", act.ActivatePaneDirection "Up"),
     keybind({ mod.l }, "l", act.ActivatePaneDirection "Right"),
+
+    -- Split panes using CTRL-A + \ or /
+    keybind({ mod.l }, "\\", act.SplitHorizontal { domain = "CurrentPaneDomain" }),
+    keybind({ mod.l }, "/", act.SplitVertical { domain = "CurrentPaneDomain" }),
+
+    -- Toggle zoom for pane
+    keybind({ mod.l }, "z", act.TogglePaneZoomState),
+
+    -- Tab management
+    keybind({ mod.l }, "c", act.SpawnTab "CurrentPaneDomain"),
+    keybind({ mod.l, mod.s }, "&", act.CloseCurrentTab { confirm = true }),
+
+    -- Close current pane
     keybind({ mod.l }, "x", act.CloseCurrentPane { confirm = true }),
+
+    -- Resize panes with CTRL-A + Shift + H/J/K/L
     keybind({ mod.l, mod.s }, "H", act.AdjustPaneSize { "Left", 5 }),
     keybind({ mod.l, mod.s }, "J", act.AdjustPaneSize { "Down", 5 }),
     keybind({ mod.l, mod.s }, "K", act.AdjustPaneSize { "Up", 5 }),
     keybind({ mod.l, mod.s }, "L", act.AdjustPaneSize { "Right", 5 }),
-    keybind({ mod.l, mod.s }, "&", act.CloseCurrentTab { confirm = true }),
+
+    -- Rename tab
     keybind(
       { mod.l },
       "e",
@@ -47,22 +57,21 @@ local keys = function()
           { Text = "Renaming Tab Title...:" },
         },
         action = callback(function(win, _, line)
-          if line == "" then
-            return
+          if line ~= "" then
+            win:active_tab():set_title(line)
           end
-          win:active_tab():set_title(line)
         end),
       }
     ),
 
-    -- workspaces
+    -- Workspaces
     keybind({ mod.l }, "w", act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" }),
 
-    -- copy and paste
+    -- Copy and paste
     keybind({ mod.c, mod.s }, "c", act.CopyTo "Clipboard"),
     keybind({ mod.c, mod.s }, "v", act.PasteFrom "Clipboard"),
 
-    -- launch spotify_player as a small pane in the bottom
+    -- Launch Spotify player as a small pane at the bottom
     keybind(
       { mod.l },
       "s",
@@ -73,7 +82,7 @@ local keys = function()
       }
     ),
 
-    -- update all plugins
+    -- Update all plugins
     keybind(
       { mod.l },
       "u",
@@ -82,34 +91,13 @@ local keys = function()
         win:toast_notification("wezterm", "plugins updated!", nil, 4000)
       end)
     ),
-
-    -- keybind(
-    --   { mod.l },
-    --   "r",
-    --   act.PromptInputLine {
-    --     description = wez.format {
-    --       { Attribute = { Intensity = "Bold" } },
-    --       { Foreground = { AnsiColor = "Teal" } },
-    --       { Text = "Enter run command: " },
-    --     },
-    --     action = callback(function(_, _, line)
-    --       if line == "" then
-    --         return
-    --       end
-    --       act.SplitPane {
-    --         direction = "Down",
-    --         command = { args = { line } },
-    --         size = { Cells = 10 },
-    --       }
-    --     end),
-    --   }
-    -- ),
   }
 
-  -- tab navigation
+  -- Tab navigation with CTRL-A + 1-9
   for i = 1, 9 do
     table.insert(keys, keybind({ mod.l }, tostring(i), act.ActivateTab(i - 1)))
   end
+
   return keys
 end
 
